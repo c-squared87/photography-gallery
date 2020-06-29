32444b5e246d5a4e9535dcaa14bdc442e61d2704
Craig Carlson <dark.gkyaun@gmail.com>
2020-06-29 16:04:56 -0500

    added basic routing capabilities

--- photography-portfolio/src/App.js
+++ photography-portfolio/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -10,12 +11,38 @@ import ContactPage from './components/ContactPage'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <PrintsPage />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/gallery">Gallery</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/gallery" component={GalleryPage} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/contact' component={ContactPage} />
+        </Switch>
+      </div>
+
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+// <li><Link to="/">Home</Link></li>
+//           <li><Link to="/about">About</Link></li>
+//           <li><Link to="/contact">Contact</Link></li>
+
+// <Route
+//         path="/events"
+//         render={props => {
+//           return <Events />;
+//         }}
+//       />
--- photography-portfolio/src/components/MainPage.js
+++ photography-portfolio/src/components/MainPage.js
@@ -5,8 +5,8 @@ function MainPage() {
     return (
         <div className="main-page">
             <div className="main-title">
-                <div className="main-title-tag"><h2 className="italic">The Photography Of</h2></div>
-                <div className="main-title-name"><h1 className="bold">Craig Carlson</h1></div>
+                <div className="main-title-tag"><h2 className="">The Photography Of</h2></div>
+                <div className="main-title-name"><h1 className="">Craig Carlson</h1></div>
             </div>
             <div className="main-page-menu">
                 <ul>
